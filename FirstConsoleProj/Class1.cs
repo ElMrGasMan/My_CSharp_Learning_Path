9c0e12394d9507a420f36d603f450a97dc4390d7
Agustin Schipani <agus-calderon-schip03@outlook.com>
2023-12-04 20:29:24 -0300

    ...

--- FirstConsoleProj/Class1.cs
+++ FirstConsoleProj/Class1.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace FirstConsoleProj
+namespace FirstNameSpace
 {
-    internal class Class1
+    class Class1
     {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Peter Griffin?");
+        }
     }
 }
--- FirstConsoleProj/Program.cs
+++ FirstConsoleProj/Program.cs
@@ -1,13 +1,2 @@
 ﻿// Apretar F11 y F10 para hacer Step In y Step Out en el debuggin. 
 
-Console.WriteLine("Hey, it's nice to see you.");
-Console.WriteLine("Wait, whats happening?...");
-
-Console.WriteLine(ChangeColor());
-
-string ChangeColor() 
-{
-    Console.ForegroundColor = ConsoleColor.Green;
-    return "I have just changed the entire color of the letters! I'm ready to make an app to rival" +
-        " Instagram.";
-}
