34424c3156cf05839e22c98379cdc818590f94a5
Agustin Schipani <agus-calderon-schip03@outlook.com>
2023-12-21 20:40:19 -0300

    Even more stuff!

---
+++ DataStructures/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace DataStructures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = new int[10];
+            int[] ints2 = [10, 20, 30, 40, 50, 61, 71, 82, 93, 104];
+            int[] ints3 = new int[5] { 10, 20, 30, 45, 55 };
+
+            ints[0] = 666;
+            ints[4] = 616;
+            ints[9] = 1337;
+
+            Console.WriteLine(ints[0]);
+            Console.WriteLine(ints[5]); //Default value is 0
+
+
+            //IMPLICIT ARRAY (using "var")
+
+            var values = new[] { 30, 40.45, 122, 12.3 }; //Possible because they all end up as double
+            //var values1 = new[] { "Joel", "Martin", 30 }; <- Not possible
+
+
+            //OBJECT ARRAYS
+
+            RandomObject[] randomObjects = new RandomObject[2];
+            RandomObject randomObject = new();
+
+            randomObjects[0] = new(false, 20003);
+            randomObjects[1] = randomObject;
+
+
+            //ANONYMUS ARRAYS
+
+            var peoples = new[]
+            {
+                new { Name = "MasterOfTheSky", Age = 17 },
+                new { Name = "WhenLegendsRise", Age = 18 },
+                new { Name = "IHaveADicotomy", Age = 19 }
+                // new { Name = "IHaveADicotomy", Age = "19" } <- Not possible, all must be identical
+            };
+        }
+    }
+
+    class RandomObject 
+    {
+        private bool randomFlag;
+        private int randomValue;
+        public bool RandomFlag { get => randomFlag; set => randomFlag = value; }
+        public int RandomValue { get => randomValue; set => randomValue = value; }
+
+        public RandomObject()
+        {
+            Random random = new Random();
+            randomFlag = Convert.ToBoolean(random.Next(0, 2));
+            randomValue = random.Next(-2333, 2333);
+        }
+
+        public RandomObject(bool randomFlag, int randomValue)
+        {
+            this.randomFlag = randomFlag;
+            this.randomValue = randomValue;
+        }
+
+        
+    }
+}
--- ExamplesOOP/Program.cs
+++ ExamplesOOP/Program.cs
@@ -34,6 +34,35 @@ namespace ExamplesOOP
             Console.WriteLine($"The first point is in the origin: {origin}.");
             Console.WriteLine($"The second point is our destiny: {destiny}.");
             Console.WriteLine($"The distance between them is: {distance}.");
+
+
+            // ANONYMOUS CLASS AND TUPLE-------------------------------------------------------------------
+
+            /* The following is a declaration of an "Anonymous class".
+             * These are used mostly when it's unnecesary to create a whole new
+             * class to define a data structure. */
+
+            var person = new { Name = "John", Age = 31, CatName = "Garfield" };
+            var anotherPerson = new { Name = "Me", Age = 45, CatName = "Pochita" };
+
+            person = anotherPerson; //Possible because they are identical. 
+
+            /* Now, Tuples and ValueTuples are a data structure that works similary to
+             * Anonymus Classes, but tuples are immutable and don't need named properties. 
+             * The diference between Tuples and ValueTuples is that ValueTuples are stored 
+             * in the stack while Tuples are stored in the heap, this means that Tuples are 
+             * reference type while ValueTuples are a value representation of them, this 
+             * makes VaueTuples slightly faster and more predictable. */
+
+            var car1 = Tuple.Create("Volkswagen", "Gol", 2006); //Tuple
+            var tank1 = (Name: "Leopard 2", "A7", 2014); //ValueTuple
+            //ValueTuples also permit named properties 
+
+            //It's not neceesary to use the "var" keyword to initialize them:
+
+            Tuple<string, string, int> car2 = new("Volkswagen", "Gol", 2005); //Tuples
+            (string name, string variant, int) tank2 = ("Leopard 2", "A7+", 2015); //ValueTuples
+
         }
     }
 }
