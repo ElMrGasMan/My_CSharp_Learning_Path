a280e64bd5544f8beebcfb65d7f7154230c2f5b9
Agustin Schipani <agus-calderon-schip03@outlook.com>
2023-12-26 22:52:59 -0300

    ...

---
+++ DataStructures/ForLoop.cs
@@ -0,0 +1,61 @@
+﻿namespace DataStructures
+{
+    internal class ForLoop
+    {
+        public static void ForLoopArr() 
+        {
+            int[] ints = [5, 2, 7, 4, 5, 6];
+
+            Console.WriteLine("First for loop----------------------------------------------");
+            for (int i = 0;  i < 4; i++) 
+            {
+                Console.WriteLine(ints[i]);
+            }
+
+            Console.WriteLine("Second for loop (using .lenght)-----------------------------");
+            for (int i = 0; i < ints.Length; i++)
+            {
+                Console.WriteLine(ints[i]);
+            }
+
+            Console.WriteLine("Descending ordered array------------------------------------");
+            Array.Sort(ints);
+            Array.Reverse(ints);
+
+            for (int i = 0; i < ints.Length; i++)
+            {
+                Console.WriteLine(ints[i]);
+            }
+
+            //FOREACH-----------------------------------------------------------------------------------
+
+            RandomObject[] randomObjects = new RandomObject[3];
+            randomObjects[0] = new();
+            randomObjects[1] = new();
+            randomObjects[2] = new();
+
+            foreach (RandomObject obj in randomObjects)
+            {
+                Console.WriteLine(obj);
+            }
+            
+        }
+
+
+        public static void PrintData(int[] data) 
+        {
+            foreach(int numb in data) 
+            {
+                Console.WriteLine(numb);
+            }
+        }
+
+        public static void ChangeData(int[] data) 
+        {
+            for (int i = 0; i < data.Length; i++)
+            {
+                data[i] = i * 66 + 1;
+            }
+        }
+    }
+}
--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -4,42 +4,54 @@
     {
         static void Main(string[] args)
         {
-            int[] ints = new int[10];
-            int[] ints2 = [10, 20, 30, 40, 50, 61, 71, 82, 93, 104];
-            int[] ints3 = new int[5] { 10, 20, 30, 45, 55 };
+            ////ARRAYS
 
-            ints[0] = 666;
-            ints[4] = 616;
-            ints[9] = 1337;
+            //int[] ints = new int[10];
+            //int[] ints2 = [10, 20, 30, 40, 50, 61, 71, 82, 93, 104];
+            //int[] ints3 = new int[5] { 10, 20, 30, 45, 55 };
 
-            Console.WriteLine(ints[0]);
-            Console.WriteLine(ints[5]); //Default value is 0
+            //ints[0] = 666;
+            //ints[4] = 616;
+            //ints[9] = 1337;
 
-
-            //IMPLICIT ARRAY (using "var")
-
-            var values = new[] { 30, 40.45, 122, 12.3 }; //Possible because they all end up as double
-            //var values1 = new[] { "Joel", "Martin", 30 }; <- Not possible
+            //Console.WriteLine(ints[0]);
+            //Console.WriteLine(ints[5]); //Default value is 0
 
 
-            //OBJECT ARRAYS
+            ////IMPLICIT ARRAY (using "var")
 
-            RandomObject[] randomObjects = new RandomObject[2];
-            RandomObject randomObject = new();
-
-            randomObjects[0] = new(false, 20003);
-            randomObjects[1] = randomObject;
+            //var values = new[] { 30, 40.45, 122, 12.3 }; //Possible because they all end up as double
+            ////var values1 = new[] { "Joel", "Martin", 30 }; <- Not possible
 
 
-            //ANONYMUS ARRAYS
+            ////OBJECT ARRAYS
 
-            var peoples = new[]
-            {
-                new { Name = "MasterOfTheSky", Age = 17 },
-                new { Name = "WhenLegendsRise", Age = 18 },
-                new { Name = "IHaveADicotomy", Age = 19 }
-                // new { Name = "IHaveADicotomy", Age = "19" } <- Not possible, all must be identical
-            };
+            //RandomObject[] randomObjects = new RandomObject[2];
+            //RandomObject randomObject = new();
+
+            //randomObjects[0] = new(false, 20003);
+            //randomObjects[1] = randomObject;
+
+
+            ////ANONYMUS ARRAYS
+
+            //var peoples = new[]
+            //{
+            //    new { Name = "MasterOfTheSky", Age = 17 },
+            //    new { Name = "WhenLegendsRise", Age = 18 },
+            //    new { Name = "IHaveADicotomy", Age = 19 }
+            //    // new { Name = "IHaveADicotomy", Age = "19" } <- Not possible, all must be identical
+            //};
+
+            ForLoop.ForLoopArr();
+            int[] ints4 = [69, 1, 20, 33, 5];
+
+            Console.WriteLine("The current array: ");
+            ForLoop.PrintData(ints4);
+            Console.WriteLine("The array is tinkered with... ");
+            ForLoop.ChangeData(ints4);
+            Console.WriteLine("The change is reflected in the original array since is passed a a reference value.");
+            ForLoop.PrintData(ints4);
         }
     }
 
@@ -63,6 +75,9 @@
             this.randomValue = randomValue;
         }
 
-        
+        public override string? ToString()
+        {
+            return $"I'm a random object with random properties such as a bool: {randomFlag}, and an int: {randomValue}.";
+        }
     }
 }
